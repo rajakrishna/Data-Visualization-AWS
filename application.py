cbca4b729a3f3d7d9d3289ad1d4f32fe1cabb4bb
Raja-Krishna <rajakrishna214@gmail.com>
2019-06-20 15:59:37 -0500

    update

--- application.py
+++ application.py
@@ -10,7 +10,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-#   return render_template("home.html")
+  return render_template("home.html")
+
+@app.route('/showdb5', methods=['GET', 'POST'])
+def showdb5():
+    cnxn = pyodbc.connect("Driver={ODBC Driver 17 for SQL Server};Server=tcp:hello1997.database.windows.net,1433;Database=quakes;Uid=raja@hello1997;Pwd={azure@123};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
+    cursor = cnxn.cursor()
+    cursor.execute("SELECT StateName from voting where TotalPop between 5000 and 10000 ")
+    row = cursor.fetchall()
+    return render_template("showdb.html", row=row)
+
+@app.route('/showdb10', methods=['GET', 'POST'])
+def showdb10():
+    cnxn = pyodbc.connect("Driver={ODBC Driver 17 for SQL Server};Server=tcp:hello1997.database.windows.net,1433;Database=quakes;Uid=raja@hello1997;Pwd={azure@123};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
+    cursor = cnxn.cursor()
+    cursor.execute("SELECT StateName from voting where TotalPop between 10000 and 50000 ")
+    row = cursor.fetchall()
+    return render_template("showdb.html", row=row)
+
 
 
 # @app.route('/magsearch', methods=['GET', 'POST'])
@@ -52,32 +69,36 @@ def home():
 #         mem.append(memdict)
 #     # a=[1,2,3,4,5]
 #     return render_template('barchart.html',a=mem,chart="bar")
-    con = pyodbc.connect("Driver={ODBC Driver 17 for SQL Server};Server=tcp:hello1997.database.windows.net,1433;Database=quakes;Uid=raja@hello1997;Pwd={azure@123};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
-    a=10
-    start=0
-    end=100
-    age_interval=[0]
-    val=start
-    while val<end:
-        val+=10
-        age_interval.append(val)
-    mem=[]
-    for i in range(0,len(age_interval)-1):
-        query="select (count(*))as count from titanic where age > "+str(age_interval[i])+" and age<"+str(age_interval[i+1])
-        cur=con.cursor()
-        cur.execute(query)
-        result=list(cur.fetchall())
-        
-        age_group=str(age_interval[i])+"-"+str(age_interval[i+1])
-        for row in result: 
-            memdict=dict()
-            for j,val in enumerate(row):
-                memdict["age_group"]=age_group  
-                memdict["count"]=val
-                mem.append(memdict)
-            print(mem)
 
-    return render_template('barchart.html',a=mem,chart="bar")
+
+
+
+    # con = pyodbc.connect("Driver={ODBC Driver 17 for SQL Server};Server=tcp:hello1997.database.windows.net,1433;Database=quakes;Uid=raja@hello1997;Pwd={azure@123};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
+    # a=10 #chnage this variable
+    # start=0
+    # end=100
+    # age_interval=[0]
+    # val=start
+    # while val<end:
+    #     val+=10
+    #     age_interval.append(val)
+    # mem=[]
+    # for i in range(0,len(age_interval)-1):
+    #     query="select (count(*))as count from titanic where age > "+str(age_interval[i])+" and age<"+str(age_interval[i+1])
+    #     cur=con.cursor()
+    #     cur.execute(query)
+    #     result=list(cur.fetchall())
+        
+    #     age_group=str(age_interval[i])+"-"+str(age_interval[i+1])
+    #     for row in result: 
+    #         memdict=dict()
+    #         for j,val in enumerate(row):
+    #             memdict["age_group"]=age_group  
+    #             memdict["count"]=val
+    #             mem.append(memdict)
+    #         print(mem)
+
+    # return render_template('barchart.html',a=mem,chart="bar")
 
 port = int(os.getenv("PORT", 5000))
 if __name__ == '__main__':
