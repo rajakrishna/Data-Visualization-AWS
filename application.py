c8f8758e3270d2f2ce9ed570d8ee44edb8661aa9
Raja-Krishna <rajakrishna214@gmail.com>
2019-06-24 17:10:30 -0500

    update

--- application.py
+++ application.py
@@ -73,34 +73,32 @@ def showdb10():
 
 @app.route('/piechart', methods=['GET', 'POST'])
 def piechart():
-    nvalue = int(request.form['nvalue'])
-    con = pyodbc.connect("Driver={ODBC Driver 17 for SQL Server};Server=tcp:hello1997.database.windows.net,1433;Database=quakes;Uid=raja@hello1997;Pwd={azure@123};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
-    # a=nvalue #chnage this variable
-    start=nvalue+990 #10
-    end=17000
-    age_interval=[0]
-    val=start
-    while val<end:
-        val+=start
-        age_interval.append(val)
+    n1 = int(request.form['n1'])
+    n2 = int(request.form['n2'])
     mem=[]
-    for i in range(0,len(age_interval)-1):
-        #query="select (count(*)) as count from voting where age > "+str(age_interval[i])+" and age<"+str(age_interval[i+1])
-        query="select count(*) as count from voting where Registered >"+str(age_interval[i])+" and Registered< "+str(age_interval[i+1])
-        cur=con.cursor()
-        cur.execute(query)
-        result=list(cur.fetchall())
-        vtt=str(age_interval[i])+"-"+str(age_interval[i+1])
-        for row in result: 
-            memdict=dict()
-            for j,val in enumerate(row):
-                memdict["vtt"]=vtt
-                memdict["States"]=val
-                mem.append(memdict)
-            print(mem)
-
-    return render_template('barchart.html',a=mem,chart="bar")
+    for y in range(n1,n2,1):
+     memdict=dict()
+     x=y*y+1 
+     memdict['x']=x
+     memdict['y']=y
+     mem.append(memdict)
+    return render_template('chart_karthik.html',a=mem,chart="scatter")
 
 port = int(os.getenv("PORT", 5000))
 if __name__ == '__main__':
     app.run(port=port,debug=True)
+
+
+
+
+
+
+      
+
+      
+      
+    
+
+
+
+
